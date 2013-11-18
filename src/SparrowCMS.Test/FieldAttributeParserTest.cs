c61a0f0ca30c90c242e2e4fd0471eda2c5787ec7
JimZheng <zhengliangjun@gmail.com>
2013-11-18 19:09:21 +0800

    update UTest

---
+++ src/SparrowCMS.Test/FieldAttributeParserTest.cs
@@ -0,0 +1,85 @@
+﻿using SparrowCMS.Base.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SparrowCMS.Base;
+using System.Collections.Generic;
+
+namespace SparrowCMS.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for FieldAttributeParserTest and is intended
+    ///to contain all FieldAttributeParserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class FieldAttributeParserTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Parse
+        ///</summary>
+        [TestMethod()]
+        public void ParseTest()
+        {
+            string labelName = string.Empty; // TODO: Initialize to an appropriate value
+            string fieldName = string.Empty; // TODO: Initialize to an appropriate value
+            string parametersTemplateContent = string.Empty; // TODO: Initialize to an appropriate value
+            IEnumerable<FieldAttribute> expected = null; // TODO: Initialize to an appropriate value
+            IEnumerable<FieldAttribute> actual;
+            actual = FieldAttributeParser.Parse(labelName, fieldName, parametersTemplateContent);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
---
+++ src/SparrowCMS.Test/LabelDescriptionParserTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using SparrowCMS.Base.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SparrowCMS.Base;
+using System.Collections.Generic;
+
+namespace SparrowCMS.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for LabelDescriptionParserTest and is intended
+    ///to contain all LabelDescriptionParserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class LabelDescriptionParserTest
+    {
+        /// <summary>
+        ///A test for Parse
+        ///</summary>
+        [TestMethod()]
+        public void ParseSelfClosedLabelDescriptionTest()
+        {
+            var templateContent = @"{System name='SiteName' /}";
+            var descs = LabelDescriptionParser.Parse(templateContent);
+            var labelDescription = descs.FirstOrDefault();
+            Assert.AreEqual("System", labelDescription.LabelName);
+        }
+
+        [TestMethod()]
+        public void ParseNormalLabelDescriptionTest()
+        {
+            var templateContent = @"
+             {Article.List top=10}
+                 <a href=""@CategoryId"">@Category.Name</a>
+                 <a href=""@id"">@(title maxlength=20) @(posttime dateformat=""yyyy-MM-dd"")</a>
+             {/Article.List}";
+
+            var labelDescription = LabelDescriptionParser.Parse(templateContent).FirstOrDefault();
+
+            Assert.AreEqual("Article.List", labelDescription.LabelName);
+
+        }
+
+        [TestMethod()]
+        public void ParseNestedLabelDescriptionTest()
+        {
+            var templateContent = @"
+         {PageLink pageSize=50 recordCount=@RecordCount}
+             <a href=""page/1"">首页</a> | <a href=""page/@prev"">上一页</a>
+                 {PageLink.List size=10}
+                     <a href=""page/@page"">@page</a>
+                 {/PageLink.List}
+             <a href=""page/@next"">下一页</a> | <a href=""page/@end"">尾页</a>
+         {/PageLink}
+";
+            var labelDescription = LabelDescriptionParser.Parse(templateContent).FirstOrDefault();
+
+            Assert.AreEqual("PageLink", labelDescription.LabelName);
+        }
+    }
+}
