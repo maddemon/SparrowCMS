6f7f8255378aafcb8be615e0432498b63655474a
JimZheng <zhengliangjun@gmail.com>
2014-02-18 16:36:05 +0800

    Document Excensions

---
+++ src/SparrowCMS.Base/Common/DocumentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparrowCMS.Core.Common
+{
+    public static class DocumentExtensions
+    {
+        public static Document ToDocument<T>(this T data)
+        {
+            var doc = new Document();
+            foreach (var p in data.GetType().GetProperties())
+            {
+                var value =  p.GetValue(data, null);
+                if (value != null && value.GetType().Module.ScopeName != "CommonLanguageRuntimeLibrary")
+                {
+                    doc[p.Name] = value.ToDocument();
+                }
+                else
+                {
+                    doc[p.Name] = value;
+                }
+            }
+            return doc;
+        }
+    }
+}
